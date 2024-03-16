d71976f407ac21665a4e944ca40014129258c460
DESKTOP-ATOV23Q\almos <almosaraaj25@gmail.com>
2024-03-16 09:53:34 +0800

    added the middleware for checking the user role.

--- backend/functions/routes/api/orders.js
+++ backend/functions/routes/api/orders.js
@@ -1,11 +1,12 @@
 const router = require("express").Router();
 const orderController = require("../../controllers/orderController");
+const { checkAdminRole } = require("../../middleware/roles/adminMiddlewareProducts");
 
 router.get("/", orderController.orderTestRouteServer);
-router.get("/all", orderController.getAllOrdersServer);
-router.get("/:orderId", orderController.getOrderByIdServer);
+router.get("/all", checkAdminRole, orderController.getAllOrdersServer);
+router.get("/:orderId", checkAdminRole, orderController.getOrderByIdServer);
 
-router.post("/create", orderController.createOrderServer);
+router.post("/create", checkAdminRole, orderController.createOrderServer);
 
 router.patch("/update/:orderId", orderController.updateOrderStatusServer);
 
