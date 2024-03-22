459f782a6bb1d8e1d8bbdaf31f5a798d878af9f0
Jovan Nadal <nadal2106086@ceu.edu.ph>
2024-03-22 20:41:01 +0800

    Update order route to use "view" instead of "update" in the URL

--- backend/functions/routes/api/orders.js
+++ backend/functions/routes/api/orders.js
@@ -9,6 +9,6 @@ router.post("/create", orderController.createOrderServer);
 
 router.patch("/update/:orderId", orderController.updateOrderStatusServer);
 
-router.get("/update/:orderId", orderController.viewCustomerOrders);
+router.get("/view/:orderId", orderController.viewCustomerOrders);
 
 module.exports = router;
