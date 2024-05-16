15fd7c790969450b6e58e4c9813111e40412b47a
danielevans254 <danielevans254@yahoo.com>
2024-05-16 18:03:42 +0800

    feat: add new routes

--- backend/functions/routes/api/cart.js
+++ backend/functions/routes/api/cart.js
@@ -1,12 +1,13 @@
 const router = require("express").Router();
 const cartController = require("../../controllers/cartController");
-const { checkIfLoggedIn } = require("../../middleware/users/sessions/checkIfLoggedIn");
+// const { checkIfLoggedIn } = require("../../middleware/users/sessions/checkIfLoggedIn");
 
 router.get("/", cartController.cartTestRouteServer);
-router.get("/:cartId", cartController.getUserCartServer);
+router.get("/:userId", cartController.getUserCartServer);
 
 router.post("/create/:userId", cartController.createCartServer);
 router.post("/add/:cartId", cartController.addToCartServer);
+router.post("/clear/:cartId", cartController.clearCartItems);
 
 router.patch("/update/:cartId/:productId", cartController.updateCartItemQuantityServer);
 // TODO: This will be invoked upon changing product options, i.e change size, addons, etc.
