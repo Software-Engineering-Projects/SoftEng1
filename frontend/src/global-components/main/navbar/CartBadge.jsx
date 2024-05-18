4fe9975671248861ea647b0a495c7908cc192ced
danielevans254 <danielevans254@yahoo.com>
2024-05-18 17:51:26 +0800

    refactor: changed how the length is fetched

--- frontend/src/global-components/main/navbar/CartBadge.jsx
+++ frontend/src/global-components/main/navbar/CartBadge.jsx
@@ -4,16 +4,13 @@ import Badge from "@mui/material/Badge";
 import { useSelector } from 'react-redux';
 // TODO: Fix the cart badge to show the number of items in the cart fetched from the backend
 export const CartBadge = () => {
-  const cartItems = useSelector((state) => state.cart.items);
-
+  const cart = useSelector((state) => state.cart)
+  console.log(cart.items.length)
+  // Fix the cart redux state if the cart is empty, just set the items to an empty array
   return (
     <Stack spacing={4} direction="row" sx={{ color: "action.active" }}>
       {/* Place holder for the cart item number data */}
-      <Badge color="primary" badgeContent={cartItems.length}
-
-        className="">
-
-      </Badge>
+      <Badge color="primary" badgeContent={cart.items ? cart.items.length : 0} />
     </Stack>
   );
 }
