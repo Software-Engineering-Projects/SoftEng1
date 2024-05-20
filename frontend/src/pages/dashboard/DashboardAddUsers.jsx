30c675c711dfd57363e7823252c76312fb916e03
danielevans254 <danielevans254@yahoo.com>
2024-05-20 09:06:14 +0800

    revert changes

--- frontend/src/pages/dashboard/DashboardAddUsers.jsx
+++ frontend/src/pages/dashboard/DashboardAddUsers.jsx
@@ -1,24 +1,5 @@
-import React, { useEffect, useState } from "react";
-import { getOrderById } from "@/api/order";
+import React from "react";
 
 export const DashboardAddUsers = () => {
-  const [order, setOrder] = useState(null);
-
-  useEffect(() => {
-    getOrderById("6MKGNbX8imV52JjaQdGk").then((data) => {
-      setOrder(data);
-    });
-  }, []);
-
-  return (
-    <div>
-      {order ? (
-        <div>
-          <p>Order ID: {order.totalPrice}</p>
-        </div>
-      ) : (
-        <p>Loading order data...</p>
-      )}
-    </div>
-  );
+  return <div>DashboardAddUsers</div>;
 };
