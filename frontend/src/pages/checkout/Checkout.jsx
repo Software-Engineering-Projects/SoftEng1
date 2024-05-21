f6f51a554d65814c59899e7614af68b436b6fff6
danielevans254 <danielevans254@yahoo.com>
2024-05-21 16:51:47 +0800

    fix: fix image render function
    fix: navlink to point to the correct link

--- frontend/src/pages/checkout/Checkout.jsx
+++ frontend/src/pages/checkout/Checkout.jsx
@@ -5,25 +5,36 @@ import { BsCashStack, BsPaypal } from "react-icons/bs";
 import { useEffect, useState } from "react";
 import { GrStripe } from "react-icons/gr";
 import { set, useForm } from "react-hook-form";
-import { NavLink, useLocation, useNavigate } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import { createOrder } from "@/api/order";
 import { useRef } from 'react';
 import { toast } from "react-hot-toast";
 import { MiniLoader } from '@/global-components/global-component-index.js';
 import { clearCartItems } from "@/api/cart";
 import { createStripeCheckoutSession } from "@/api/checkout";
+import { Logo } from '@/public/images/public-images-index.js';
+import { setCartItems } from "../../context/actions/cartAction";
+import { getUserCart } from '@/api/cart'
+import { Loader } from "@/global-components/global-component-index.js";
+
 // FIXME: Since the state of this isn't intialized here it throws and error if you reload within this page with visiting the root
 export const Checkout = () => {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
   // FIXME:
   const cartItems = useSelector((state) => state.cart.items.items);
   const cart = useSelector((state) => state.cart.items);
   const user = useSelector((state) => state.user);
 
+  if (!cartItems || cartItems.length === 0 || !user) {
+    navigate('/menu', { replace: true });
+  }
+
   const [selectedPayment, setSelectedPayment] = useState('');
   const [orderButtonLoading, setOrderButtonLoading] = useState(false);
   const [orderDataList, setOrderDataList] = useState({});
   const [paymentError, setPaymentError] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
 
   const cartId = cart.cartId;
   const userId = user.uid;
@@ -37,44 +48,36 @@ export const Checkout = () => {
 
   const paymentMethodRef = useRef(null);
 
-  // TODO: Add validation for the fields
-
-  // TODO: Add validation, if no payment method is selected, show an error message, if no contact information is provided, show an error message, if no shipping information is provided, show an error message
-  // TODO: Save this to the database after with the default
-
-  // FIXME: Also fetch the cart items and set as the line items in the orderDataList object
-  // TODO: Setup the shippingAddress and make sure the user object already has this information inputted in the database
-  // TODO: Make sure to properly assign the line items which are based on the cart items
-  const lineItems = cartItems.map(item => ({
-    productId: item.productId,
-    productIdentifier: item.productIdentifier,
-    productQuantity: item.productQuantity,
-    productPrice: item.productPrice,
-    productName: item.productName,
-  }));
-
   useEffect(() => {
-    const orderData = {
-      userId: userId,
-      cartId: cartId,
-      paymentChannel: selectedPayment,
-      customerName: customerName,
-      customerEmail: customerEmail,
-      totalPrice: totalPrice,
-      // TODO: Properly destructure the lineItems from the cart items from redux
-      // TODO: This will be hardcoded for now, but it isn't actually required for the validation, im planning to create a gate, to not allow users to order until they've set an address and necessary information
-      shippingAddress: {
-        country: "Philippines",
-        city: "Metro Manila",
-        state: "NCR",
-        postalCode: "1000",
-        line1: "1234 Street",
-        line2: "Unit 101",
-      },
-      lineItems: lineItems
-    };
-    setOrderDataList(orderData);
-  }, [userId, cartId, selectedPayment, customerName, customerEmail, totalPrice]);
+    // Only proceed when loading is false
+    if (!isLoading) {
+      const orderData = {
+        userId: userId,
+        cartId: cartId,
+        paymentChannel: selectedPayment,
+        customerName: customerName,
+        customerEmail: customerEmail,
+        totalPrice: totalPrice,
+        // TODO: This will be hardcoded for now, but it isn't actually required for the validation, im planning to create a gate, to not allow users to order until they've set an address and necessary information
+        shippingAddress: {
+          country: "Philippines",
+          city: "Metro Manila",
+          state: "NCR",
+          postalCode: "1000",
+          line1: "1234 Street",
+          line2: "Unit 101",
+        },
+        lineItems: cartItems.map(item => ({
+          productId: item.productId,
+          productIdentifier: item.productIdentifier,
+          productQuantity: item.productQuantity,
+          productPrice: item.productPrice,
+          productName: item.productName,
+        }))
+      };
+      setOrderDataList(orderData);
+    }
+  }, [isLoading, userId, cartId, selectedPayment, customerName, customerEmail, totalPrice, cartItems]);
 
   const validatePaymentSelection = () => {
     if (!selectedPayment) {
@@ -102,60 +105,66 @@ export const Checkout = () => {
   // TODO: Add Joi to validate the order data but it should also be based on whether or not the payment method is stripe or cash
   // FIXME: The url isn't being returned correctly for some reason.. even the api call is correct, i just forgot to import axios... tf
   // FIXME: If an error happens during stripe properly reset the loading button state
-  const paymentCheckoutPage = (selectedPayment) => {
+  // REVIEW:
+  const paymentCheckoutPage = async (selectedPayment) => {
     setOrderButtonLoading(true);
 
-    if (selectedPayment === 'stripe') {
-      console.log("Redirect to Stripe [PAYMENT CHECKOUT PAGE]");
-      createStripeCheckoutSession(cartId, userId)
-        .then((url) => {
-          if (url) {
-            // todo: only clear when the payment is successful
-            // clearCartItems(cartId);
-            createOrder(orderDataList);
-            window.location.href = url;
-          } else {
-            throw new Error("Failed to create Stripe checkout session");
-          }
-        })
-        .catch((error) => {
-          console.error("An error occurred during Stripe checkout:", error);
-          toast.error(error.message);
-        })
-        .finally(() => {
-          setOrderButtonLoading(false);
-        });
-    } else if (selectedPayment === 'cash') {
-      createOrder(orderDataList)
-        .then((result) => {
-          if (result) {
-            clearCartItems(cartId);
-            navigate('/checkout/success', { replace: true });
-          } else {
-            throw new Error("Failed to create order");
-          }
-        })
-        .catch((error) => {
-          console.error("An error occurred during cash payment processing:", error);
-          toast.error(error.message);
-        })
-        .finally(() => {
-          setOrderButtonLoading(false);
-        });
+    try {
+      if (selectedPayment === 'stripe') {
+        console.log("Redirect to Stripe [PAYMENT CHECKOUT PAGE]");
+        const url = await createStripeCheckoutSession(cartId, userId);
+        if (url) {
+          window.location.href = url;
+        } else {
+          throw new Error("Failed to create Stripe checkout session");
+        }
+      } else if (selectedPayment === 'cash') {
+        const result = await createOrder(orderDataList);
+        if (result) {
+          setIsLoading(true);
+          navigate('/checkout/success', { replace: true });
+        } else {
+          throw new Error("Failed to create order");
+        }
+      }
+      await clearCartItems(cartId);
+      const fetchedCart = await getUserCart(user.uid);
+      dispatch(setCartItems(fetchedCart.data));
+    } catch (error) {
+      console.error("An error occurred during payment processing:", error);
+      toast.error(error.message);
+    } finally {
+      setOrderButtonLoading(false);
     }
+
   };
+
+  const ImageComponent = ({ src, alt }) => {
+    const imageSrc = src || Logo;
+    return (
+      <img src={imageSrc} alt={alt} className="h-full w-full object-cover object-center" />
+    );
+  };
+
+  if (isLoading) {
+    return <Loader variant="cart" className="text-black h-screen w-full" />;
+  }
 
   return (
     <>
       <div className="gap-8 mx-auto px-16 py-4" >
         <div className="flex flex-col items-center">
-          <h1 className="text-3xl font-bold my-6"> Checkout</h1>
+          <h1 className="text-3xl font-bold my-6">
+            Checkout
+          </h1>
         </div>
         <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex flex-wrap -mx-4">
             <div className="w-full lg:w-1/2 px-4 mb-6 lg:mb-0">
               <div ref={paymentMethodRef} className="bg-white shadow rounded-lg p-6">
-                <h2 className="text-xl font-semibold mb-4 text-gray-800">Payment Method</h2>
+                <h2 className="text-xl font-semibold mb-4 text-gray-800">
+                  Payment Method
+                </h2>
                 <div className="grid grid-cols-1 gap-y-4">
                   {['stripe', 'cash'].map((paymentMethod) => (
                     <label
@@ -180,7 +189,9 @@ export const Checkout = () => {
                     </label>
                   ))}
                 </div>
-                {paymentError && <p className="text-red-500 text-md font-bold mt-2">{paymentError}</p>}
+                {paymentError && <p className="text-red-500 text-md font-bold mt-2">
+                  {paymentError}
+                </p>}
               </div>
             </div>
             <div className="w-full lg:w-1/2 px-4 shadow-lg">
@@ -193,16 +204,12 @@ export const Checkout = () => {
                     {cartItems.map((product) => (
                       <li key={product.productId} className="flex items-center space-x-4">
                         <div className="flex-shrink-0 w-20 h-20 overflow-hidden rounded-md border border-gray-300">
-                          <img
-                            src={product.image}
-                            alt={product.imageAlt}
-                            className="w-full h-full object-cover object-center"
-                          />
+                          <ImageComponent src={product.imageUrl} alt={product.productName} />
                         </div>
                         <div className="flex-grow">
                           <p className="text-lg font-semibold text-gray-900">
-                            <NavLink href={product.href} className="hover:underline">
-                              {product.name}
+                            <NavLink to={`/menu/${product.productId}`} className="hover:underline">
+                              {product.productName}
                             </NavLink>
                           </p>
                           <div className="flex flex-row justify-between items-center gap-6 p-6 bg-white">
@@ -249,83 +256,6 @@ export const Checkout = () => {
               </div>
             </div>
           </div>
-
-          {/* <div className="py-12 flex flex-col space-y-4">
-            <h2 className="text-2xl font-semibold mb-4 text-gray-700 col-span-2 mt-auto flex flex-col items-center justify-center">
-              Customer Information
-            </h2>
-            <label className="text-gray-600 font-medium" htmlFor="mobile-number">
-              Mobile Number
-            </label>
-            <input
-              className="rounded-lg border-gray-300"
-              id="mobile-number"
-              placeholder="Enter your mobile number"
-              type="tel"
-            />
-            <label className="text-gray-600 font-medium" htmlFor="name">
-              Name
-            </label>
-            <input
-              className="rounded-lg border border-gray-300 p-2 focus:outline-none focus:ring focus:border-blue-500"
-              id="name"
-              placeholder="Enter your name"
-            />
-          </div>
-          <div className="py-4 flex flex-col space-y-4">
-            <h2 className="text-2xl font-semibold mb-4 text-gray-700 col-span-2 mt-auto flex flex-col items-center justify-center">
-              Shipping Information
-            </h2>
-            <label className="text-gray-600 font-medium" htmlFor="address-1">
-              Address Line 1
-            </label>
-            <input
-              className="rounded-lg border border-gray-300 p-2 focus:outline-none focus:ring focus:border-blue-500"
-              id="address-1"
-              placeholder="House Number etc"
-            />
-            <label className="text-gray-600 font-medium" htmlFor="address-2">
-              Address Line 2
-            </label>
-            <input
-              className="rounded-lg border border-gray-300 p-2 focus:outline-none focus:ring focus:border-blue-500"
-              id="address-2"
-              placeholder="Street etc"
-            />
-            <label className="text-gray-600 font-medium" htmlFor="city">
-              City
-            </label>
-            <input
-              className="rounded-lg border border-gray-300 p-2 focus:outline-none focus:ring focus:border-blue-500"
-              id="city"
-              placeholder="Enter your city"
-            />
-            <label className="text-gray-600 font-medium" htmlFor="state">
-              State
-            </label>
-            <input
-              className="rounded-lg border border-gray-300 p-2 focus:outline-none focus:ring focus:border-blue-500"
-              id="state"
-              placeholder="Enter your state"
-            />
-            <label className="text-gray-600 font-medium" htmlFor="country">
-              Country
-            </label>
-            <input
-              className="rounded-lg border border-gray-300 p-2 focus:outline-none focus:ring focus:border-blue-500"
-              id="country"
-              placeholder="Enter your country"
-            />
-            <label className="text-gray-600 font-medium" htmlFor="zip-code">
-              Zip Code
-            </label>
-            <input
-              className="rounded-lg border border-gray-300 p-2 focus:outline-none focus:ring focus:border-blue-500"
-              id="zip-code"
-              placeholder="Enter your zip code"
-            />
-          </div> */}
-
         </div>
       </div>
       <div className="flex flex-col items-center justify-center">
