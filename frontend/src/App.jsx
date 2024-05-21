8dd81f58e05e822c63c49fad1c40347fe3b1371f
danielevans254 <danielevans254@yahoo.com>
2024-05-21 16:47:15 +0800

    Refactor useEffect hooks and optimize API actions in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useCallback } from "react";
 import './index.css'
 import { Route, Routes } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
@@ -6,7 +6,7 @@ import { app } from "./config/firebase.config.js";
 import { useDispatch, useSelector } from "react-redux";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
-import { setUserDetails, setUserNull } from "./context/actions/userActions.js";
+import { setUserDetails } from "./context/actions/userActions.js";
 import { Loader } from "@/global-components/global-component-index.js";
 import AllRoutes from "./routes.jsx";
 import { setRoleType, setRoleNull } from "./context/actions/userRoleAction";
@@ -24,10 +24,7 @@ const App = () => {
   const [loadingCount, setLoadingCount] = useState(0);
   const user = useSelector((state) => state.user);
   const productsList = useSelector((state) => state.product)
-
   const firebaseAuth = getAuth();
-
-  // TODO: Separate useEffect hooks into a different file
 
   const addLoading = () => {
     setLoadingCount((count) => count + 1);
@@ -39,43 +36,6 @@ const App = () => {
       return Math.max(0, count - 1);
     });
   };
-
-  useEffect(() => {
-    const fetchProductsList = async () => {
-      if (productsList.products.length === 0) {
-        addLoading();
-        try {
-          const fetchedProductsList = await getAllProducts();
-          if (fetchedProductsList) {
-            console.log(fetchedProductsList);
-            dispatch(getProducts(fetchedProductsList));
-          }
-        } catch (error) {
-          console.error('Failed to fetch products:', error);
-        } finally {
-          removeLoading();
-        }
-      }
-    };
-    fetchProductsList();
-  }, [dispatch, productsList.products.length]);
-
-  useEffect(() => {
-    const fetchCart = async () => {
-      if (user) {
-        addLoading();
-        try {
-          const fetchedCart = await getUserCart(user.uid);
-          dispatch(setCartItems(fetchedCart.data));
-        } catch (error) {
-          console.error('Failed to fetch cart:', error);
-        } finally {
-          removeLoading();
-        }
-      }
-    };
-    fetchCart();
-  }, [user, dispatch]);
 
   useEffect(() => {
     const fetchRole = async () => {
@@ -108,8 +68,43 @@ const App = () => {
     });
   }, [dispatch, firebaseAuth, navigate, role]);
 
-  const showLoader = isLoading && loadingCount > 0;
+  useEffect(() => {
+    const fetchProductsList = async () => {
+      if (productsList.products.length === 0) {
+        addLoading();
+        try {
+          const fetchedProductsList = await getAllProducts();
+          if (fetchedProductsList) {
+            dispatch(getProducts(fetchedProductsList));
+          }
+        } catch (error) {
+          console.error('Failed to fetch products:', error);
+        } finally {
+          removeLoading();
+        }
+      }
+    };
+    fetchProductsList();
+  }, [dispatch, productsList.products.length]);
 
+  useEffect(() => {
+    const fetchCart = async () => {
+      if (user) {
+        addLoading();
+        try {
+          const fetchedCart = await getUserCart(user.uid);
+          dispatch(setCartItems(fetchedCart.data));
+        } catch (error) {
+          console.error('Failed to fetch cart:', error);
+        } finally {
+          removeLoading();
+        }
+      }
+    };
+    fetchCart();
+  }, [user, dispatch]);
+
+  const showLoader = isLoading && loadingCount > 0;
 
   return (
     <div>
