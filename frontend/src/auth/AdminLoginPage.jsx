45fa8ee3c0c27c7ed206f18ca6cb9491a51c8ea1
danielevans254 <danielevans254@yahoo.com>
2024-05-23 17:25:15 +0800

    Refactor image directory and fix login functionality

--- frontend/src/auth/AdminLoginPage.jsx
+++ frontend/src/auth/AdminLoginPage.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { LoginBG, Logo } from '../../public/images/public-images-index.js';
+import { LoginBG, Logo } from '@/public/images/public-images-index.js';
 import { app } from '../config/firebase.config.js';
 import { useNavigate } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
@@ -9,12 +9,12 @@ import {
   getAuth,
   signInWithEmailAndPassword,
 } from 'firebase/auth';
-import { NavLink } from 'react-router-dom';
+import { fetchRole } from '@/api/user.js';
 
 export const AdminLoginPage = () => {
-  const [email, setEmail] = useState('example@example.com');
+  const dispatch = useDispatch();
+  const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [isFormValid, setIsFormValid] = useState(false);
 
   const navigate = useNavigate();
   const firebaseAuth = getAuth(app);
@@ -24,24 +24,63 @@ export const AdminLoginPage = () => {
     try {
       const userCredential = await signInWithEmailAndPassword(firebaseAuth, email, password);
       const user = userCredential.user;
-      if (user) {
+      const role = await fetchRole(user.uid);
+      if (user && role === 'user') {
         dispatch(setUserDetails(user));
         dispatch(setUserName(user.displayName));
-        toast.success('Logged in successfully');
-        navigate('/admin/dashboard');
+        navigate('/dashboard');
+        setTimeout(() => {
+          toast.success('Logged in successfully');
+        }, 2000);
+      } else {
+        toast.error('You are not an admin');
       }
     } catch (error) {
-      toast.error(error.message);
+      console.error(error);
+      switch (error.code) {
+        case 'auth/email-already-in-use':
+          toast.error('Email already exists');
+          break;
+        case 'auth/invalid-email':
+          toast.error('Invalid email format');
+          break;
+        default:
+          toast.error('Something went wrong');
+          break;
+      }
     }
   };
 
   return (
-    <div className='flex items-center justify-center'>
-      <form onSubmit={handleLogin}>
-        <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
-        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
-        <button type="submit" disabled={!isFormValid}>Sign In</button>
+    <div className="flex items-center justify-center h-screen bg-gray-200">
+      <form className="p-6 bg-white rounded shadow-md" onSubmit={handleLogin}>
+        <h1 className="mb-6 text-3xl font-bold text-center mr-4">
+          <img src={Logo} alt="Logo" className="w-12 h-12 inline-block mr-4" />
+          Admin Login
+        </h1>
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Email"
+          className="w-full p-2 mb-6 text-black border-b-2 border-blue-500 outline-none focus:bg-gray-300"
+          required
+        />
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Password"
+          className="w-full p-2 mb-6 text-black border-b-2 border-blue-500 outline-none focus:bg-gray-300"
+          required
+        />
+        <button
+          type="submit"
+          className={`w-full px-4 py-2 text-white rounded bg-blue-500 hover:bg-blue-700}`}
+        >
+          Sign In
+        </button>
       </form>
     </div>
   );
-};
+}
